d1fb0694a466351b25be126c82a84a232c247f45
4leyam <aleyam4ndroid@gmail.com>
2022-06-21 12:59:03 +0530

    Initial Commit

---
+++ brainyDxInterview/brainyDxInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainyDxInterviewApp.swift
+//  brainyDxInterview
+//
+//  Created by Aleyam  Rich on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct brainyDxInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
